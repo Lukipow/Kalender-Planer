d5779f0cc249f4fbec1d4438b21faf824a070774
Jeffrey057 <udjeffrey.jay@gmail.com>
2023-07-11 01:17:56 +0200

    added login + registratio + Design

--- wochenplaner.sql
+++ wochenplaner.sql
@@ -1,18 +1,29 @@
 CREATE TABLE nutzer (
- user TEXT NOT NULL PRIMARY KEY,
+ "user" TEXT NOT NULL PRIMARY KEY,
  passwort TEXT  NOT NULL
 );
 
+
 CREATE TABLE planer (
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- planer_name TEXT NOT NULL,
- user TEXT NOT NULL,
- oeffentlich TEXT NOT NULL,
- schluessel TEXT
+  id INTEGER PRIMARY KEY,
+  planer_name TEXT NOT NULL,
+  "user" TEXT NOT NULL,
+  oeffentlich TEXT NOT NULL,
+  schluessel TEXT
 );
 
+
 CREATE TABLE eintraege (
- id INTEGER PRIMARY KEY AUTOINCREMENT,
+ id INTEGER PRIMARY KEY,
  eintrag TEXT NOT NULL,
  datum DATE NOT NULL
 );
+
+CREATE TABLE Users (
+    id INT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    password VARCHAR(50)
+);
+
+INSERT INTO Users (username, password)
+VALUES ('jeffrey@gmail.com', '123456789');
